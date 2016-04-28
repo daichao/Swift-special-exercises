d8a0f54e2e85d41532af0cab13fb136d27f51cb9
daichao <daichao@daichaodeiMac.local>
2016-04-28 16:57:41 +0800

    update

---
+++ AppCoda_BeginningiOS9ProgrammingwithSwift/FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by daichao on 16/4/28.
+//  Copyright © 2016年 com.daichao.*. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    
+    
+    
+}
---
+++ AppCoda_BeginningiOS9ProgrammingwithSwift/FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by daichao on 16/4/28.
+//  Copyright © 2016年 com.daichao.*. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats",
+        "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina",
+        "Donostia", "Royal Oak", "Thai Cafe"]
+    
+    var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg",
+            "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg",
+            "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg",
+            "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg",
+            "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg",
+            "donostia.jpg", "royaloak.jpg", "thaicafe.jpg"]
+    
+   override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+                super.viewDidLoad()
+                self.tableView.separatorStyle = .None
+    }
+    
+   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+            return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cellIdentifier="Cell"
+            let cell=tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! RestaurantTableViewCell
+            
+            cell.nameLabel?.text=restaurantNames[indexPath.row]
+            cell.thumbnailImageView?.image=UIImage(named: restaurantImages[indexPath.row])
+            cell.accessoryType = .DisclosureIndicator
+            return cell
+    }
+}
